2c94efceb36e108e1918bd0eab19ffc3934af072
afnanshihab <afnan.shihab@yahoo.com>
2021-06-09 02:06:58 +0300

    its my abaut me guessing game

--- js/app.js
+++ js/app.js
@@ -10,13 +10,12 @@ let aboutAge = prompt('Do you think I am under 30?').toLowerCase();
 
 if (aboutAge === 'yes' || aboutAge === 'y') {
     alert('this is a correct answer i am 28 ');
-
-    console.log(`Your answer is  ${aboutAge} and it is correct`);
+    //console.log(`Your age is  ${aboutAge} and it is correct`);
 
 } else if (aboutAge === 'no' || aboutAge === 'n') {
     alert('wrong answer, try to guess in the next question');
 } else {
-    alert('Pease type (yes) or (no) or (y) or (n)');
+    alert('Please type (yes) or (no) or (y) or (n)');
 
 
 }
@@ -27,12 +26,12 @@ let aboutLikes = prompt('Do you think I like to drink coffe?').toLowerCase();
 if (aboutLikes === 'yes' || aboutLikes === 'y') {
     alert('this is a correct answer actully I prefer coffee more than anything else ');
 
-    console.log(`Your answer is  ${aboutLikes} and it is correct`);
+    //console.log(`Your answer is  ${aboutLikes} and it is correct`);
 
 } else if (aboutLikes === 'no' || aboutLikes === 'n') {
     alert('wrong answer, try to guess in the next question');
 } else {
-    alert('Pease type (yes) or (no) or (y) or (n)');
+    alert('Please type (yes) or (no) or (y) or (n)');
 
 
 }
@@ -44,12 +43,12 @@ let aboutstudy = prompt('Do you think I am Graphic designer?').toLowerCase();
 if (aboutstudy === 'no' || aboutstudy === 'n') {
     alert('this is a correct answer im not a designer i am an engineer ');
 
-    console.log(`Your answer is  ${aboutstudy} and it is correct`);
+    //console.log(`Your answer is  ${aboutstudy} and it is correct`);
 
 } else if (aboutstudy === 'yes' || aboutstudy === 'y') {
     alert('wrong answer, try to guess in the next question');
 } else {
-    alert('Pease type (yes) or (no) or (y) or (n)');
+    alert('Please type (yes) or (no) or (y) or (n)');
 
 
 
@@ -62,12 +61,12 @@ let aboutLive = prompt('Do you think I live in Canada?').toLowerCase();
 if (aboutLive === 'no' || aboutLive === 'n') {
     alert('this is a correct answer i live in Jordan right now but i hope to go to Canada ');
 
-    console.log(`Your answer is  ${aboutLive} and it is correct`);
+    //console.log(`Your answer is  ${aboutLive} and it is correct`);
 
 } else if (aboutLive === 'yes' || aboutLive === 'y') {
     alert('wrong answer, try to guess in the next question');
 } else {
-    alert('Pease type (yes) or (no) or (y) or (n)');
+    alert('Please type (yes) or (no) or (y) or (n)');
 
 
 
@@ -80,12 +79,12 @@ let abouOrigin = prompt('Do you think I come from Jordan ?').toLowerCase();
 if (abouOrigin === 'no' || abouOrigin === 'n') {
     alert('this is a correct answer, actully i came from Palestine ');
 
-    console.log(`Your answer is  ${abouOrigin} and it is correct`);
+    //console.log(`Your answer is  ${abouOrigin} and it is correct`);
 
 } else if (abouOrigin === 'yes' || abouOrigin === 'y') {
     alert('wrong answer, try to guess in the next question');
 } else {
-    alert('Pease type (yes) or (no) or (y) or (n)');
+    alert('Please type (yes) or (no) or (y) or (n)');
 }
 
 
